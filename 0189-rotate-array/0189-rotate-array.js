b1ad22ef6161ac10d2f9738d316cfda6a211bd77
Faranaz08 <83759275+Faranaz08@users.noreply.github.com>
2024-05-12 20:11:21 +0530

    Time: 78 ms (70.13%), Space: 56.1 MB (95.74%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.js
+++ 0189-rotate-array/0189-rotate-array.js
@@ -4,19 +4,40 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function(nums, k) {
-    //rotate to right 
-    let ans=new Array(nums.length);
-         let n=nums.length;
-         k=k%n;
-         for(let i=(n-k);i<n;i++){
-            ans[i-(n-k)] =nums[i];
-         }
-         for(let i=k;i<n;i++){
-             ans[i]=nums[i-k];
-         }
-         for(let i=0;i<n;i++){
-             nums[i]=ans[i];
-         }
-    return ans;
+    // //rotate to right 
+    // let ans=new Array(nums.length);
+    //      let n=nums.length;
+    //      k=k%n;
+    //      for(let i=(n-k);i<n;i++){
+    //         ans[i-(n-k)] =nums[i];
+    //      }
+    //      for(let i=k;i<n;i++){
+    //          ans[i]=nums[i-k];
+    //      }
+    //      for(let i=0;i<n;i++){
+    //          nums[i]=ans[i];
+    //      }
+    // return ans;
+    
+    
+    // better approach 
+    
+    k=k%nums.length;
+    let n=nums.length;
+  
+
+    let reverse=function(nums,start,end){
+        while(start<end){
+            let temp=nums[start];
+            nums[start]=nums[end];
+            nums[end]=temp;
+            start++;
+            end--;
+        }
+    }
+      reverse(nums,0,n-1);
+    reverse(nums,0,k-1);
+    reverse(nums,k,n-1);
+    return nums;
     
 };
