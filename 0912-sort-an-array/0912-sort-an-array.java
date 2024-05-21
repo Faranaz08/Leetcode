a0cd701cbdd07d6690c4ee22adf364e368d1323d
Faranaz08 <83759275+Faranaz08@users.noreply.github.com>
2024-05-21 06:45:24 +0530

    Time: 27 ms (60.73%), Space: 56.3 MB (45.24%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -2,25 +2,28 @@ class Solution {
     public int[] sortArray(int[] nums) {
         if(nums.length==1){
             return nums;
+           
         }
-        int mid=nums.length/2;
-        int[] left=sortArray(Arrays.copyOfRange(nums,0,mid));
-        int[] right=sortArray(Arrays.copyOfRange(nums,mid,nums.length));
-
+        int start=0,end=nums.length;
+        int mid=start+(end-start)/2;
+         int[] left=sortArray(Arrays.copyOfRange(nums,0,mid));
+        int[] right=sortArray(Arrays.copyOfRange(nums,mid,end));
         return merge(left,right);
     }
-    private static int[] merge(int[] first,int[] second){
+    public int[] merge(int[] first ,int[] second){
         int[] mix=new int[first.length+second.length];
         int i=0,j=0,k=0;
         while(i<first.length && j<second.length){
             if(first[i]<second[j]){
                 mix[k]=first[i];
                 i++;
+               
             }else{
                 mix[k]=second[j];
                 j++;
+                
             }
-            k++;
+             k++;
         }
         while(i<first.length){
             mix[k]=first[i];
