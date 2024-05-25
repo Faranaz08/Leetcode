abc752b86065af53c5f642bc7e4ae92a8da7712c
Faranaz08 <83759275+Faranaz08@users.noreply.github.com>
2024-05-25 06:46:26 +0530

    Time: 1 ms (58.09%), Space: 41.7 MB (52.18%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -23,16 +23,16 @@ class Solution {
         
        //
         
-        HashSet<Character> jewelSet=new HashSet<>();
-        for(char j:jewels.toCharArray()){
-            jewelSet.add(j);
-        }int count=0;
+     HashSet<Character> set=new HashSet<>();
+        for(char c:jewels.toCharArray()){
+            set.add(c);
+        }
+        int count=0;
         for(char s:stones.toCharArray()){
-            if(jewelSet.contains(s)){
+            if(set.contains(s)){
                 count++;
             }
         }
-       return count;
-
+return count;
     }
 }
