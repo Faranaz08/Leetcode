3e38b67b798a07db1d85306bb11028b730f8a6c7
Faranaz08 <83759275+Faranaz08@users.noreply.github.com>
2024-08-15 18:21:56 +0530

    Time: 6 ms (36.28%), Space: 43.5 MB (20.98%) - LeetHub

--- 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.java
+++ 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.java
@@ -1,24 +1,19 @@
 class Solution {
-    public int findLucky(int[] nums) {
-          HashMap<Integer,Integer> map=new HashMap<>();
+    public int findLucky(int[] arr) {
+        HashMap<Integer,Integer> hm=new HashMap<>();
         int max=-1;
-        for(int i=0;i<nums.length;i++){
-            if(map.containsKey(nums[i])){
-                map.put(nums[i],map.get(nums[i])+1);
+        for(int i=0;i<arr.length;i++){
+            if(hm.containsKey(arr[i])){
+                hm.put(arr[i],hm.get(arr[i])+1);
             }else{
-                map.put(nums[i],1);
+                hm.put(arr[i],1);
             }
         }
-        for(Map.Entry<Integer,Integer> e: map.entrySet()){
-            if(e.getKey()==e.getValue()){
-                max=Math.max(max,e.getValue());
-            }
-        }
+       for(Map.Entry<Integer,Integer> ent:hm.entrySet()){
+           if(ent.getKey()==ent.getValue()){
+              max=Math.max(max,ent.getValue());
+           }
+       }
         return max;
     }
 }
-
-
-//2-2 key value
-//3-1
-//map.get(key)==map.get(value)
