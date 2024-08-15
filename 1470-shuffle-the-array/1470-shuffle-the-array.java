9b4495189edea4b039a5d0bce7f2dc3b711e257a
Faranaz08 <83759275+Faranaz08@users.noreply.github.com>
2024-08-15 12:24:12 +0530

    Time: 0 ms (100.00%), Space: 44.5 MB (74.27%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -1,16 +1,16 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-       int[] ans=new int[nums.length];
+        int[] arr=new int[nums.length];
         int x=0;
         for(int i=0;i<nums.length;i++){
             if(i%2==0){
-                ans[i]=nums[x];
+                arr[i]=nums[x];
                 x++;
             }else{
-                ans[i]=nums[n];
-                n++;
+                arr[i]=nums[n];
+                n++;//already given pointer
             }
         }
-        return ans;
+        return arr;
     }
 }
