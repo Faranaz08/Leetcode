9a987b2ab977d2a7dd717896ae25200c79f31e9a
Faranaz08 <83759275+Faranaz08@users.noreply.github.com>
2024-08-17 21:40:00 +0530

    Time: 0 ms (100.00%), Space: 44 MB (24.63%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
@@ -8,23 +8,9 @@
  */
 class Solution {
     public void deleteNode(ListNode node) {
-        
         ListNode Nextnode=node.next;
-        node.val=node.next.val;
+        node.val=Nextnode.val;
         node.next=Nextnode.next;
         Nextnode.next=null;
-        
-        
-        
-        
-        
-        
-        
-        
-        // // givrn node not head
-        // ListNode Nextnode=node.next;//will store next node
-        // node.val=Nextnode.val;//pasting nextnode value into the node
-        // node.next=Nextnode.next;////changing address from node to next of nextnode
-        // Nextnode.next=null;
     }
 }
