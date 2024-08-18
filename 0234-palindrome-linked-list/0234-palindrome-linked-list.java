571c3d97eddff57570cd4376caa57bcf093dc19e
Faranaz08 <83759275+Faranaz08@users.noreply.github.com>
2024-08-18 14:24:01 +0530

    Time: 3 ms (99.69%), Space: 68.5 MB (34.61%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,48 +10,23 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        if(head==null || head.next==null){
-            return true;
-        }
-        
-        // finding middle of the linkledlist
-        ListNode slow=head;
-        ListNode fast=head;
-        while(fast.next!=null && fast.next.next!=null){
+        ListNode slow=head ,fast=head;
+        while(fast!=null && fast.next!=null){
             slow=slow.next;
             fast=fast.next.next;
         }
-        
-        //reverse the second half 
-        ListNode newHead=reverse(slow.next);
-        
-        //compare first and second half
-        ListNode firstHead=head;
-        ListNode secondHead=newHead;
-        while(secondHead!=null){
-            if(firstHead.val!=secondHead.val){
-                reverse(newHead);
-                return false;
-            }
-            firstHead=firstHead.next;
-            secondHead=secondHead.next;
-        }
-        reverse(newHead);
-        return true;
-    }
-    private ListNode reverse(ListNode head){
-        if(head==null || head.next==null){
-            return head;
-        }
         ListNode prev=null;
-        ListNode curr=head;
-        ListNode front=head;
-        while(curr!=null){
-            front=curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=front;
+        while(slow!=null){
+            ListNode front=slow.next;
+            slow.next=prev;
+            prev=slow;
+            slow=front;
         }
-        return prev;
+        while(prev!=null){
+            if(head.val!=prev.val) return false;
+            head=head.next;
+            prev=prev.next;
+        }
+        return true;
     }
 }
